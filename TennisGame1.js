160bb245e1434bcb38ed711b9bb4015bbce77493
Will Brindle <will.brindle@bazaarvoice.com>
2020-07-15 19:45:31 +0100

    Return early

--- TennisGame1.js
+++ TennisGame1.js
@@ -16,25 +16,32 @@ class TennisGame1 {
     let tempScore = 0;
     if (this.player1.score.value === this.player2.score.value) {
       if (this.player1.score.value < 3) {
-        score = `${this.player1.score.label}-All`;
-      } else {
-        score = 'Deuce';
+        return `${this.player1.score.label}-All`;
       }
-    } else if (this.player1.score.value >= 4 || this.player2.score.value >= 4) {
+      return 'Deuce';
+    }
+
+    if (this.player1.score.value >= 4 || this.player2.score.value >= 4) {
       const minusResult = this.player1.score.value - this.player2.score.value;
-      if (minusResult === 1) score = 'Advantage player1';
-      else if (minusResult === -1) score = 'Advantage player2';
-      else if (minusResult >= 2) score = 'Win for player1';
-      else score = 'Win for player2';
-    } else {
-      for (let i = 1; i < 3; i += 1) {
-        if (i === 1) tempScore = this.player1.score;
-        else {
-          score += '-';
-          tempScore = this.player2.score;
-        }
-        score += tempScore.label;
+      if (minusResult === 1) {
+        return 'Advantage player1';
       }
+      if (minusResult === -1) {
+        return 'Advantage player2';
+      }
+      if (minusResult >= 2) {
+        return 'Win for player1';
+      }
+      return 'Win for player2';
+    }
+
+    for (let i = 1; i < 3; i += 1) {
+      if (i === 1) tempScore = this.player1.score;
+      else {
+        score += '-';
+        tempScore = this.player2.score;
+      }
+      score += tempScore.label;
     }
     return score;
   }
